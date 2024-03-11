cabd1bf1bf96fde021008513a622d72ad2e8e0ce
drazen04 <hangtime23@hotmail.it>
2024-03-11 16:11:18 +0100

    revert fake tutorial

--- tutorials.js
+++ tutorials.js
@@ -276,10 +276,5 @@ export const tutorials = [
         name: "Katana extension",
         type: [muscleGroups.triceps],
         video_url: "https://www.instagram.com/p/CtOM5IkskhZ/"
-    },
-    {
-        name: "Curl al cavo alto",
-        type: [muscleGroups.biceps],
-        video_url: ""
     }
 ]
